d851eba6c19f943fd978ad0aad23a95a983f8310
James Petry <gavilancomun@gmail.com>
2015-05-29 14:56:11 +0100

    data

--- Kakuro.hs
+++ Kakuro.hs
@@ -12,18 +12,16 @@ e = Empty
 a = Across
 d = Down
 da = DownAcross
-v = Value [1 .. 9]
+v = Value [1..9]
 vv values = Value values
 
 draw (Empty) = "   -----  "
 draw (Down n) =  printf "   %2d\\--  " n
 draw (Across n) = printf "   --\\%2d  " n
 draw (DownAcross down across) = (printf "   %2d" down) ++ printf "\\%2d  " across
-draw (Value values) = 
-      if 1 == length values then 
-        concatMap (\ x -> "     " ++ show x ++ "    ") values
-      else 
-         " " ++ concatMap (\ x -> if x `elem` values then show x else ".") [ 1..9 ]
+draw (Value values) 
+     | 1 == (length values) = concatMap (\ x -> "     " ++ show x ++ "    ") values
+     | otherwise = " " ++ concatMap (\ x -> if x `elem` values then show x else ".") [1..9]
 
 drawRow row = (concatMap draw row) ++ "\n"
 
@@ -31,21 +29,19 @@ drawGrid grid = "\n" ++ concatMap drawRow grid
 
 allDifferent nums = (length nums) == length (nub nums)
 
-permute vs target soFar = 
-    if target >= 1 then 
+permute vs target soFar  
+    | target >= 1 = 
         if length soFar == (length vs) - 1 then [ soFar ++ [ target ] ]
         else 
             case vs !! (length soFar) of
               Value values -> concatMap (\ v -> permute vs (target - v) (soFar ++ [ v ])) values
               _ -> []
-    else []
+    | otherwise = []
 
 permuteAll vs total = permute vs total []
 
-isPossible cell n =
-  case cell of
-    Value values -> n `elem` values
-    _ -> False
+isPossible (Value values) n = n `elem` values
+isPossible cell _ = False
 
 solveStep cells total = 
     let final = (length cells) - 1 in
@@ -110,6 +106,7 @@ grid1 = [ [ e, (d 4), (d 22), e, (d 16), (d 3) ],
           [ (a 15), v, v, (a 12), v, v ] ]
 
 main = do
+  putStrLn (show $ permuteAll [v, v] 6)
   putStrLn (draw Empty)
   putStrLn (draw (Across 9))
   putStrLn (draw (DownAcross 9 9))
