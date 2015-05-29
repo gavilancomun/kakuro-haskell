884d68dee295c87c6d667eb7de8d9b04ead48d88
James Petry <gavilancomun@gmail.com>
2015-05-29 14:26:17 +0100

    data

--- Kakuro.hs
+++ Kakuro.hs
@@ -48,6 +48,62 @@ isPossible cell n =
     Value values -> n `elem` values
     _ -> False
 
+solveStep cells total = 
+    let final = (length cells) - 1 in
+    map (\ p -> Value(p))
+        (transpose
+          (filter allDifferent
+            (filter (\ p -> isPossible (cells !! final) (p !! final)) (permuteAll cells total))))
+
+rowTarget (Across n) = n
+rowTarget (DownAcross _ a) = a
+rowTarget _ = 0
+
+colTarget (Down d) = d
+colTarget (DownAcross d _) = d
+colTarget _ = 0
+
+solvePair f (nvs : vs : _) = nvs ++ (solveStep vs (f (last nvs))) 
+solvePair f (nvs : _) = nvs
+solvePair f pair = []
+
+solvePairRow = solvePair rowTarget
+
+solvePairCol = solvePair colTarget
+
+partitionBy f [] = []
+partitionBy f coll@(x : xs) = 
+        let fx = f x
+            run = takeWhile (\ y -> fx == f y) coll
+        in
+        [ run ] ++ (partitionBy f (drop (length run) coll))
+
+partitionAll n step [] = []
+partitionAll n step coll = [(take n coll)] ++ (partitionAll n step (drop step coll))
+
+partitionN n coll = partitionAll n n coll
+
+isValue (Value _) = True
+isValue _ = False
+
+solveLine f cells = concatMap f (partitionN 2 (partitionBy isValue cells))
+
+solveRow = solveLine solvePairRow
+
+solveCol = solveLine solvePairCol
+
+solveGrid grid = 
+  transpose (map solveCol (transpose (map solveRow grid)))
+
+solver :: [[Cell]] -> [[Cell]]
+solver grid =
+  let g = solveGrid grid
+  in
+  if g == grid then
+    grid
+  else
+    solver g
+
 grid1 = [ [ e, (d 4), (d 22), e, (d 16), (d 3) ],
           [ (a 3), v, v, (da 16 6), v, v ],
           [ (a 18), v, v, v, v, v ],
@@ -62,3 +118,5 @@ main = do
   putStrLn (draw v)
   putStrLn (draw (vv [1, 3, 5, 9]))
   putStrLn (drawGrid grid1)
+  putStrLn (drawGrid (solver grid1))
+
