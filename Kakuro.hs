65ac6540af1a4393ce00e5ddc41e810dc1e07780
James Petry <gavilancomun@gmail.com>
2015-05-29 13:10:03 +0100

    data

--- Kakuro.hs
+++ Kakuro.hs
@@ -1,3 +1,4 @@
+import Text.Printf
 
 data Cell = Empty
             | Across Int
@@ -12,6 +13,20 @@ da = DownAcross
 v = Value [1 .. 9]
 vv values = Value values
 
+draw (Empty) = "   -----  "
+draw (Down n) =  printf "   %2d\\--  " n
+draw (Across n) = printf "   --\\%2d  " n
+draw (DownAcross down across) = (printf "   %2d" down) ++ printf "\\%2d  " across
+draw (Value values) = 
+      if 1 == length values then 
+        concatMap (\ x -> "     " ++ show x ++ "    ") values
+      else 
+         " " ++ concatMap (\ x -> if any (== x) values then show x else ".") [ 1..9 ]
+
 main = do
-  putStrLn "---"
+  putStrLn (draw Empty)
+  putStrLn (draw (Across 9))
+  putStrLn (draw (DownAcross 9 9))
+  putStrLn (draw v)
+  putStrLn (draw (vv [1, 3, 5, 9]))
 
