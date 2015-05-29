4dc00fa5970c8bba6bcb50c565acd4672c141d5a
James Petry <gavilancomun@gmail.com>
2015-05-29 13:32:20 +0100

    data

--- Kakuro.hs
+++ Kakuro.hs
@@ -1,4 +1,5 @@
 import Text.Printf
+import Data.List
 
 data Cell = Empty
             | Across Int
@@ -21,12 +22,31 @@ draw (Value values) =
       if 1 == length values then 
         concatMap (\ x -> "     " ++ show x ++ "    ") values
       else 
-         " " ++ concatMap (\ x -> if any (== x) values then show x else ".") [ 1..9 ]
+         " " ++ concatMap (\ x -> if x `elem` values then show x else ".") [ 1..9 ]
 
 
 drawRow row = (concatMap draw row) ++ "\n"
 
 drawGrid grid = "\n" ++ concatMap drawRow grid
+
+allDifferent nums = (length nums) == length (nub nums)
+
+
+permute vs target soFar = 
+    if target >= 1 then 
+        if length soFar == (length vs) - 1 then [ soFar ++ [ target ] ]
+        else 
+            case vs !! (length soFar) of
+              Value values -> concatMap (\ v -> permute vs (target - v) (soFar ++ [ v ])) values
+              _ -> []
+    else []
+
+permuteAll vs total = permute vs total []
+
+isPossible cell n =
+  case cell of
+    Value values -> n `elem` values
+    _ -> False
 
 grid1 = [ [ e, (d 4), (d 22), e, (d 16), (d 3) ],
           [ (a 3), v, v, (da 16 6), v, v ],
